5b356cff5325b5ef8520fb08930ef6ed04a8843e
Krzysztof Szymański <kjszymanski@gmail.com>
2014-11-18 13:38:44 +0100

    parameters fix

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -3,7 +3,8 @@ if (isset($_ENV['CLEARDB_DATABASE_URL'])) {
     $db = parse_url($_ENV['CLEARDB_DATABASE_URL']);
     $container->setParameter('database_driver', 'pdo_mysql');
     $container->setParameter('database_host', $db['host']);
-    $container->setParameter('database_port', $db['port']);
+    if (isset($db['port']))
+        $container->setParameter('database_port', $db['port']);
     $container->setParameter('database_name', trim($db['name'], '/'));
     $container->setParameter('database_user', $db['user']);
     $container->setParameter('database_password', $db['pass']);
